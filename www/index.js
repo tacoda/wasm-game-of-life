c2378530c8b1f9c66d676baf4dbbbc12a164b439
Ian Johnson <tacoda@pm.me>
2019-03-11 21:08:20 -0400

    Initial implementation

--- www/index.js
+++ www/index.js
@@ -1,3 +1,12 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet("Bob");
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
